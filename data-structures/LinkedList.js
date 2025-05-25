3a200a8e462075cd5b40290a1314d91cda996989
tomas <thomas.kravcik@gmail.com>
2025-05-25 14:02:25 +0200

    Rename LinkedList to DoublyLinkedList and add `prev` support.
    Updated the data structure to support previous node references (`prev`), enabling bidirectional traversal. Added necessary adjustments to append, prepend, insert, and remove methods for maintaining `prev` links correctly.

--- data-structures/LinkedList.js
+++ data-structures/LinkedList.js
@@ -2,14 +2,16 @@ class Node {
     constructor(value) {
         this.value = value;
         this.next = null;
+        this.prev = null;
     }
 }
 
-class LinkedList {
+class DoublyLinkedList {
     constructor(value) {
         this.head = {
             value,
-            next: null
+            next: null,
+            prev: null
         }
         this.tail = this.head;
         this.length = 1;
@@ -17,6 +19,7 @@ class LinkedList {
 
     append(value) {
         const node = new Node(value);
+        node.prev = this.tail
         this.tail.next = node;
         this.tail = node;
         this.length++;
@@ -26,6 +29,7 @@ class LinkedList {
     prepend(value) {
         const node = new Node(value);
         node.next = this.head;
+        this.head.prev = node;
         this.head = node;
         this.length++;
         return this;
@@ -38,11 +42,13 @@ class LinkedList {
         //get reference of leader node
         const leaderNode = this.traverseToIndex(index - 1);
         //save reference of next node
-        const holdingPointer = leaderNode.next;
+        const follower = leaderNode.next;
         //update leader node
         leaderNode.next = node;
         //update next node with saved reference
-        node.next = holdingPointer;
+        node.next = follower;
+        node.prev = leaderNode;
+        follower.prev = node;
         this.length++;
         return this;
     }
@@ -58,11 +64,11 @@ class LinkedList {
         const leaderNode = this.traverseToIndex(index - 1);
         //get reference to the node we want to remove
         const unwantedNode = leaderNode.next;
+        unwantedNode.next.prev = leaderNode;
         //update leader node's next pointer to skip the unwanted node
         leaderNode.next = unwantedNode.next;
         this.length--;
         return this
-        
     }
 
 
@@ -76,6 +82,35 @@ class LinkedList {
         }
         return currentNode;
     }
+
+    reverse() {
+        //if there is only one node, return the list
+        if (!this.head.next) {
+            return this;
+        }
+        //set first to head and tail to head
+        let first = this.head;
+        this.tail = this.head;
+        //set second to node after first
+        let second = first.next;
+        //iterate until second is null
+        while (second) {
+            //save reference to next node
+            let temp = second.next;
+            //reverse pointer to point backwards
+            second.next = first;
+            //move first forward
+            first = second;
+            //move second forward using saved reference 
+            second = temp;
+        }
+        //set original head's next to null since its now tail
+        this.head.next = null;
+        //update head to the last node
+        this.head = first;
+        return this;
+    }
+
     printList() {
         let currentNode = this.head;
         const arr = [];
