ce04767d1916d6cae7250a8eb1cf7c5460ae2e44
tomas <thomas.kravcik@gmail.com>
2025-07-06 19:04:54 +0200

    Add basic IndexedDB example with object store creation and data insertion

--- Storage API/indexedDB.js
+++ Storage API/indexedDB.js
@@ -1,1 +1,27 @@
 //low level API, can be saved lot of data
+//not sql, but similar
+//init
+const open = indexedDB.open('users', 1);
+
+open.onupgradeneeded = () => {
+    const db = open.result;
+    db.createObjectStore('users', { keyPath: 'id' });
+}
+
+open.onsuccess = () => {
+    console.log('success myDB');
+    const db = open.result;
+    const transaction = db.transaction("users", "readwrite")
+    const store = transaction.objectStore("users");
+
+    store.put({
+        id: 1,
+        name: "jon",
+        age: 99
+    })
+}
+
+open.onerror = () => {
+    console.error('error myDB');
+}
+
