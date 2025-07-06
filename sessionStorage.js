68236fc22f69322941771a1d7b418e499d0c5393
tomas <thomas.kravcik@gmail.com>
2025-07-06 18:53:37 +0200

    Add session storage example with basic operations
    Added a new `sessionStorage.js` file demonstrating session storage usage with examples of setting, getting, removing, and clearing session storage items. Updated HTML to include the new script.

---
+++ sessionStorage.js
@@ -0,0 +1,7 @@
+//sessions end closing tabs or browser
+//simple caching, 5mb
+//form data temp save
+sessionStorage.setItem('name', 'textValeu');
+sessionStorage.getItem('name');
+// sessionStorage.removeItem('name');
+// sessionStorage.clear();
