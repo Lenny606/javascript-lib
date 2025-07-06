d3f7afb4e8e27d62152f329fafb195793c248edd
tomas <thomas.kravcik@gmail.com>
2025-07-06 18:26:44 +0200

    Add basic local storage example with theme toggle
    Introduced a simple HTML file demonstrating local storage usage. Added a JavaScript implementation for theme toggling (light/dark mode) and local storage clear functionality.

---
+++ localStorage.js
@@ -0,0 +1,35 @@
+//everything is saved as String
+
+localStorage.setItem('name', 'zhangsan');
+localStorage.setItem('int', 123);
+localStorage.setItem('arr', [1,2,3,4]); //type modified , use strigify
+localStorage.setItem('arr', JSON.stringify([1,2,3,4])); //better, then parse
+
+
+//DARK THEME
+initTheme()
+const btn = document.querySelector('.theme-btn');
+const btnClear = document.querySelector('.clear-btn');
+btn.addEventListener('click', () => {
+    toggleTheme()
+
+});
+btnClear.addEventListener('click', () => {
+    localStorage.clear();
+
+});
+function toggleTheme() {
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'light');
+        document.body.classList.remove('dark');
+    } else {
+        localStorage.setItem('theme', 'dark');
+        document.body.classList.add('dark');
+    }
+}
+function initTheme(){
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        document.body.classList.add('dark');
+    }
+}
